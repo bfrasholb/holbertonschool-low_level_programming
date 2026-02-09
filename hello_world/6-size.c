fc9c021ee70c2578b7950ab471b71683ba675b27
bfrasholb <13065@holbertonstudents.com>
2026-02-09 09:27:41 +0000

    Grandeur Script

---
+++ hello_world/6-size.c
@@ -0,0 +1,25 @@
+/**
+ * File: 6-size.c
+ * bfrasholb
+ * Prints Type Sizes
+ */
+
+#include <stdio.h>
+
+/**
+ * main -
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	printf("Size of a char: %lu.\n", sizeof(char));
+	printf("Size of an int: %lu.\n", sizeof(int));
+	printf("Size of a long int: %lu.\n", sizeof(long int));
+	printf("Size of a long long int: %lu.\n", sizeof(long long int));
+	printf("Size of a float: %lu.\n", sizeof(float));
+
+	return (0);
+}
