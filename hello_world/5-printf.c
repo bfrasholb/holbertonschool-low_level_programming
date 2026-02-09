87182bb0712405fb60be4f924c2f108054ec40fa
bfrasholb <13065@holbertonstudents.com>
2026-02-09 08:44:31 +0000

    Betty wasn't Happy

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ *A Short Script
+ * main: prints a statement
+ * return: 0
+ */
+
 int main(void)
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 	}
