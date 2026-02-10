22d3893e18817ae71f3b40485b81db7edcebcdad
bfrasholb <13065@holbertonstudents.com>
2026-02-10 01:23:29 +0000

    Fixed the Script

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,19 @@
 int main(void)
 {
 int n;
-srand(time(NULL));
-n = rand();
-if (n >= 0)
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n == 0)
 {
-printf("%d is positive\n", n);
+printf("%d is 0\n", n);
+}
+else if (n <= 0)
+{
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is negative\n", n);
+printf("%d is positive\n", n);
 }
 return (0);
 }
