9e3249b55aebc965998cdb0057e5f722b65214da
bfrasholb <13065@holbertonstudents.com>
2026-02-11 08:23:51 +0000

    Corrected Script Formatting

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,28 +15,22 @@ int a = 0;
 		while (b <= 9)
 		{
 			int c = a * b;
-			if (b == 0)
-				{
-				  _putchar(c + '0');
-				}
+		        if (c < 10)
+			{
+			  _putchar(c + '0');
+			}
 			else
-				{
-				_putchar(',');
-				_putchar(' ');
-				if (c < 10)
-					{
-					  _putchar(' ');
-					}
-				else
-				  {
-				    _putchar((c / 10) + '0');
-				    _putchar((c % 10) + '0');
-				  }
-				b++;
-				}
+			  {
+			   _putchar((c / 10) + '0');
+			   _putchar((c % 10) + '0');
+			  }
+			_putchar(',');
+			_putchar(' ');
+		       	b++;
+				
 		}
 	       	_putchar('\n');
-       		a++;
+			a++;
 		
 	}
 }
