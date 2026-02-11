f8dafe87540068851ff8e92422a9a7ec8988a5a7
bfrasholb <13065@holbertonstudents.com>
2026-02-11 09:28:41 +0000

    rough draft

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_to_98-script prints from input to 98
+ *@n:integer
+ *
+ */
+
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+while (n <=98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+n++;
+}
+}
+}
+else
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+n--;
+}
+}
+}
+printf("\n");  
+}
