9e820a0b5cabc9c1bf9b3b0095cada0a39dbe8d4
bfrasholb <13065@holbertonstudents.com>
2026-02-11 09:09:09 +0000

    Added an Add function + Updated main

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add-function adds integers together
+ *@a:integer
+ *@b:integer
+ *Return:returns the sum of a + b
+ *
+ */
+
+int add(int a, int b)
+{
+int c = a + b;
+return (c);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -11,5 +11,6 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
+int add(int, int);
 void times_table(void);
 #endif
