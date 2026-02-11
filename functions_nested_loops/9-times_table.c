951262a54e6b3316ac4c52cef98ccbb2260db3b8
bfrasholb <13065@holbertonstudents.com>
2026-02-11 09:02:57 +0000

    Working times table and updated main

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,36 +1,38 @@
 #include "main.h"
 
 /**
- *
- *
+ *times_table-function prints 0-9*0-9
  *
  */
 
 void times_table(void)
 {
-int a = 0;
-	while (a <= 9)
-	{
-		int b = 0;
-		while (b <= 9)
-		{
-			int c = a * b;
-		        if (c < 10)
-			{
-			  _putchar(c + '0');
-			}
-			else
-			  {
-			   _putchar((c / 10) + '0');
-			   _putchar((c % 10) + '0');
-			  }
-			_putchar(',');
-			_putchar(' ');
-		       	b++;
-				
-		}
-	       	_putchar('\n');
-			a++;
-		
-	}
+int a = 0, b, c;
+while (a <= 9)
+{
+b = 0;
+while (b <= 9)
+{
+c = a * b;
+if (b == 0)
+{
+_putchar(c + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+if (c < 10)
+{
+_putchar(' ');
+}
+else
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+b++;
+}
+_putchar('\n');
+a++;
+}
 }
