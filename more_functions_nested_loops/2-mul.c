fe17def03698b64efe28ddf6327deb8cfba15d1a
bfrasholb <13065@holbertonstudents.com>
2026-02-16 00:31:43 +0000

    Added mul, updated main

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *mul-description.
+ *@a:an integer
+ *@b:an integer
+ *Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+int c = a * b;
+return (c);
+}
