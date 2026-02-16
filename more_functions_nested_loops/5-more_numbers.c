24300129529516063d08f7f80fe0b3209116e1d4
bfrasholb <13065@holbertonstudents.com>
2026-02-16 07:19:27 +0000

    MOOOOAARRRR

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,6 @@
 
 /**
  *more_numbers-description.
- *
  *Return: 0
  *
  */
@@ -14,10 +13,21 @@ int j = 0;
 while (j < 10)
 {
 int i = 0;
-while (i < 10)
+while (i < 15)
+{
+int a = i / 10;
+int b = i % 10;
+if (a == 0)
 {
 _putchar(i + '0');
 i++;
+}
+else
+{
+_putchar(a + '0');
+_putchar(b + '0');
+i++;
+}
 }
 _putchar('\n');
 j++;
