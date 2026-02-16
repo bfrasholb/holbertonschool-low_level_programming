ab3d98edc61c383bbe0451464c55bee0945841fa
bfrasholb <13065@holbertonstudents.com>
2026-02-16 08:35:38 +0000

    BumbleDrink

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ *main-description
+ *Return:0 when complete.
+ */
+
+int main(void)
+{
+int i = 1;
+while (i <= 100)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+i++;
+}
+if (i % 3 == 0)
+{
+printf("Fizz ");
+i++;
+}
+if (i % 5 == 0)
+{
+printf("Buzz ");
+i++;
+}
+else
+{
+printf("%d ", i);
+i++;
+}
+}
+printf("\n");
+return (0);
+}
