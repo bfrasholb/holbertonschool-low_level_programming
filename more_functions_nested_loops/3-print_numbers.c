58970d0c211c260e321e8b1a3b58f9948ad1d6c3
bfrasholb <13065@holbertonstudents.com>
2026-02-16 01:17:49 +0000

    Updated main, added print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_numbers-description.
+ *@i:an integer
+ *Return: 0
+ *
+ */
+
+void print_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
