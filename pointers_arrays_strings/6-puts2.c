8894907b685ac587648b906742d42df7947163f4
bfrasholb <13065@holbertonstudents.com>
2026-02-17 02:19:54 +0000

    Betty yelled again

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,20 +2,21 @@
 #include "main.h"
 
 /**
- *
+ *puts2-prints odd characters in a string
+ *@str:string input
  */
 
 void puts2(char *str)
 {
-  int i = 0;
-  while (str[i] != '\0')
-    {
-      if (i % 2 == 0)
-	{
-	  _putchar(str[i]);
-	}
-      i++;
-    }
-  _putchar('\n');
+int i = 0;
 
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n');
 }
