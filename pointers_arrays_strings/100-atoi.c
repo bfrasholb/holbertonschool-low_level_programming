a55812258b09d8954ad0d69077034d6a0fe1b841
bfrasholb <13065@holbertonstudents.com>
2026-02-17 04:42:02 +0000

    closer ?

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,26 +2,30 @@
 #include "main.h"
 
 /**
- *
- */
+*
+*/
 
 int _atoi(char *s)
 {
-  int i = 0;
-  int digits = 0;
-  int sign = 1;
-  
-  while (s[i] != '\0')
-    {
-      if ((s[i] <= '9') && (s[i] >= '0'))
-	{
-	  digits = digits * 10 + (s[i] - '0');
-	}
-      else if (s[i] == '-')
-	{
-	  sign *= -1;
-	}
-      i++;
-    }
-  return (digits * sign);
+int i = 0;
+int digits = 0;
+int sign = 1;
+int first_digit = 0;
+
+while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+{
+i++;
+}
+while ((s[i] == '-') && !first_digit)
+{
+sign *= -1;
+i++;
+}
+while (s[i] >= '0' && s[i] <= '9')
+{
+digits = digits * 10 + (s[i] - '0');
+first_digit = 1;
+i++;
+}
+return (digits * sign);
 }
