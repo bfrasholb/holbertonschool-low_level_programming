8f6af8b35760c6fb376effd379faed2f955fc306
bfrasholb <13065@holbertonstudents.com>
2026-02-17 05:25:57 +0000

    Thats a big number

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+#include <limits.h>
 
 /**
- *_atoi-function returns the first integer in a string as type int
- *@s:input string
- *Return:returns the first number as type int
- */
+*_atoi-function returns the first integer in a string as type int
+*@s:input string
+*Return:returns the first number as type int
+*/
 
 int _atoi(char *s)
 {
@@ -22,8 +23,23 @@ sign *= -1;
 }
 else if (s[i] >= '0' && s[i] <= '9')
 {
-digits = digits * 10 + (s[i] - '0');
+int overflow = s[i] - '0';
 first_digit = 1;
+{
+if (sign == 1)
+{
+if (digits > (INT_MAX - overflow) / 10)
+return INT_MAX;
+}
+else
+{
+if (digits > (-(INT_MIN + overflow)) / 10)
+{
+return INT_MIN;
+}
+}
+digits = digits * 10 + (s[i] - '0');
+}
 }
 else if (first_digit)
 {
