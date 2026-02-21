0614759af99c11af0f14f4a6c9b8f20c9f035323
bfrasholb <13065@holbertonstudents.com>
2026-02-21 07:10:31 +0000

    Cap,N

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,46 +2,43 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- */
+*cap_string-sets  the firs letter after each separator to a capital.
+*@str:input string.
+*Return: returns the corrected string.
+*/
 
 char *cap_string(char *str)
 {
-  char sep[] = {'!', '"', '(', ')', ',', '.', ';', '?', '{', '}'};
-  int i,j,k = 0;
-  int b;
+int i = 0;
+int b;
 
-  while (str[i] != '\0')
-    {
-      b = 0;
-      while (sep[j] != '\0')
-	{
-	  if (str[i] == sep[j])
-	    {
-	      b = 1;
-	      break;
-	    }
-	  j++;
-	}
-      while (b == 1)
-	{
-	  k = i;
-	  while (str[k] != '\0')
-	    {
-	      if ((str[k] >= 'a' && str[k] <= 'z') || (str[k] >= 'A' && str[k] <= 'Z'))
-		{
-		  if (str[k] >= 'a' && str[k] <= 'z')
-		    {
-		      str[i] = str[i] - 32;
-		    }
-		  b = 0;
-		}
-	      k++;
-	    }
-	}
-      i++;
-    }
-  return (str);
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z' && b == 1)
+{
+str[i] = str[i] - 32;
+}
+b = 0;
+
+if (
+str[i] == ' ' ||
+str[i] == ',' ||
+str[i] == ';' ||
+str[i] == '.' ||
+str[i] == '!' ||
+str[i] == '?' ||
+str[i] == '"' ||
+str[i] == '(' ||
+str[i] == ')' ||
+str[i] == '{' ||
+str[i] == '}' ||
+str[i] == '\t' ||
+str[i] == '\n'
+)
+{
+b = 1;
+}
+i++;
+}
+return (str);
 }
