8a058e980e76a93fdcce3dfc9253cd4e603e406c
bfrasholb <13065@holbertonstudents.com>
2026-02-23 06:30:48 +0000

    Autoupdate

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,16 +9,22 @@
 unsigned int _strspn(char *s, char *accept)
 {
   int i = 0;
-
+  char *a;
+  
   while (*s)
     {
-      while (*accept)
+      a = accept;
+      while (*a)
 	{
-	  if (*s == *accept)
+	  if (*s == *a)
 	   {
 	     i++;
 	   }
+	  a++;
 	}
+      if (*a == '\0')
+	return (i);
+      s++;
     }
   return (i);
 }
