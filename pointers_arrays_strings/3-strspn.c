fe768315bff3dba228f57b2b770654e2acbd8dec
bfrasholb <13065@holbertonstudents.com>
2026-02-23 06:38:50 +0000

    Autoupdate

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,29 +2,31 @@
 #include <stdio.h>
 
 /**
- *
- *
- */
+*_strspn-returns a count of characters in s after the first match in accept
+*@s:a string
+*@accept:a string
+*Return:returns the count
+*/
 
 unsigned int _strspn(char *s, char *accept)
 {
-  int i = 0;
-  char *a;
-  
-  while (*s)
-    {
-      a = accept;
-      while (*a)
-	{
-	  if (*s == *a)
-	   {
-	     i++;
-	   }
-	  a++;
-	}
-      if (*a == '\0')
-	return (i);
-      s++;
-    }
-  return (i);
+int i = 0;
+char *a;
+
+while (*s)
+{
+a = accept;
+while (*a)
+{
+if (*s == *a)
+{
+i++;
+}
+a++;
+}
+if (*a == '\0')
+return (i);
+s++;
+}
+return (i);
 }
