57fcc2c7a0de9c97a8771aedefafe2ec43b8ae74
bfrasholb <13065@holbertonstudents.com>
2026-02-23 04:48:33 +0000

    autoupdate main

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -18,5 +18,5 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *_memset(char *s, char b, unsigned int n);
-*_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
