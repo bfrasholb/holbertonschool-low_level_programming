d85d2adee34269e4f91b7dcbd087684893f02062
bfrasholb <13065@holbertonstudents.com>
2026-02-23 07:09:23 +0000

    Autoupdate

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,34 +2,36 @@
 #include <stdio.h>
 
 /**
- *
- *
- */
+*_strpbrk-returns a pointer to the first match (x E accept) E s
+*@s:a string
+*@accept:a string
+*Return:returns a pointer
+*/
 
 char *_strpbrk(char *s, char *accept)
 {
-  char *a;
-  char *p;
-  int stop = 0;
-  
-  while (*s)
-    {
-      a = accept;
-      while (*a)
-	{
-	  if (*a == *s)
-	    {
-	      p = s;
-	      stop = 1;
-	      break;
-	    }
-	  a++;
-	}
-      if (stop)
-	{
-	  break;
-	}
-      s++;
-    }
-  return (p);
+char *a;
+char *p;
+int stop = 0;
+
+while (*s)
+{
+a = accept;
+while (*a)
+{
+if (*a == *s)
+{
+p = s;
+stop = 1;
+break;
+}
+a++;
+}
+if (stop)
+{
+break;
+}
+s++;
+}
+return (p);
 }
