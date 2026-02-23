85dec6cff973a13c3f25e1d9dc0d0d1cfdf977a5
bfrasholb <13065@holbertonstudents.com>
2026-02-23 07:35:54 +0000

    Autoupdate

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -2,40 +2,47 @@
 #include <stdio.h>
 
 /**
- *
- *
- */
+*_strstr-function points to the first matching instance of needle in haystack
+*@haystack:a string
+*@needle:a string
+*Return:returns pointer to the first instance match
+*/
 
 char *_strstr(char *haystack, char *needle)
 {
-  char *n;
-  int ln = 0; /** length of needle */
-  int match;
-  char *h;
-  
-  while (*needle)
-    {
-      ln++;
-      needle++;
-    }
-  needle -= ln;
-  
-  while (*haystack)
-    {
-      h = haystack;
-      n = needle;
-      match = 0;
-      while (*n)
-	{
-	  match++;
-	  n++;
-	  h++;
-	}
-      if (match == ln)
-	    {
-	      return (h);
-	    }
-      haystack++;
-    }  
+char *n;
+int ln = 0; /** length of needle */
+int match;
+char *h;
+char *start;
+
+while (*needle)
+{
+ln++;
+needle++;
+}
+needle -= ln;
+
+while (*haystack)
+{
+h = haystack;
+n = needle;
+match = 0;
+while (*n)
+{
+if (*h == *n)
+{
+match++;
+}
+n++;
+h++;
+}
+if (match == ln)
+{
+start = h - ln;
+return (start);
+}
+haystack++;
+}
 return (NULL);
 }
