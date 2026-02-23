f6dd9d7cde9a5707a18c865b8e8acb383873e032
bfrasholb <13065@holbertonstudents.com>
2026-02-23 05:49:51 +0000

    Autoupdate

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_memset-Overwrites a buffer of length with a constant
+ *@s:a pointer to an address to start the buffer
+ *@b:a constant
+ *@n:length of the buffer
+ *Return:returns the overwritten buffer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+while (n--)
+{
+*dest++ = *src++;
+}
+return (dest);
+}
