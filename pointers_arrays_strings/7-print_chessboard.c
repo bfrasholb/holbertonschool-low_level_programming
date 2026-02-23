fd557bdaa1d40c9c616d31dc8efa6cfe8371b9d7
bfrasholb <13065@holbertonstudents.com>
2026-02-23 07:46:42 +0000

    Autoupdate

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,23 +2,24 @@
 #include "main.h"
 
 /**
- *
- *
- *
- */
+*print_chessboard-printsan 8x8 array
+*@a:an 8x8 array
+*/
 
 void print_chessboard(char (*a)[8])
 {
-  int i, j;
+int i = 0;
+int j;
 
-  while (i < 8)
-    {
-      while (j < 8)
-	{
-	  putchar(a[i][j]);
-	  j++;
-	  }
-      putchar('\n');
-      i++;
-    }
+while (i < 8)
+{
+j = 0;
+while (j < 8)
+{
+putchar(a[i][j]);
+j++;
+}
+putchar('\n');
+i++;
+}
 }
