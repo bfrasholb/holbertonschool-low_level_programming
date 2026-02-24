6798895c3fdcf1f5bc2013b708b1534e9fc788cd
bfrasholb <13065@holbertonstudents.com>
2026-02-24 00:54:54 +0000

    Autoupdate

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 
 /**
- *_strlen_recursion-returns string length
- *@s:a string
- *Return:returns string length.
- */
+*_strlen_recursion-returns string length
+*@s:a string
+*Return:returns string length.
+*/
 
 int _strlen_recursion(char *s)
 {
-  int i = 0;
-    
-  if (*s == '\0')
-    {
-      return (i);
-    }
-  if (*s != '\0')
-    {
-      i++;
-      i += _strlen_recursion(++s);
-    }
+int i = 0;
+
+if (*s == '\0')
+{
+return (i);
+}
+if (*s != '\0')
+{
+i++;
+i += _strlen_recursion(++s);
+}
 return (i);
 }
