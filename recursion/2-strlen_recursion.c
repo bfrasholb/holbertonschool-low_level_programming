f33a0037c46ec80c813d67e7d3445fb73c9deaed
bfrasholb <13065@holbertonstudents.com>
2026-02-24 01:04:26 +0000

    Autoupdate

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,16 +9,9 @@
 
 int _strlen_recursion(char *s)
 {
-int i = 0;
-
 if (*s == '\0')
 {
-return (i);
+return (0);
 }
-if (*s != '\0')
-{
-i++;
-i += _strlen_recursion(++s);
-}
-return (i);
+return (1 + _strlen_recursion(s + 1));
 }
