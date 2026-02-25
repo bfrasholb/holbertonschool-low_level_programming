34a17e0ae8bfe333b247032a11af733f44835f01
bfrasholb <13065@holbertonstudents.com>
2026-02-25 01:15:27 +0000

    Autoupdate

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main-prints the product of any 2 arguments, throws error if argc != 3
+ *@argc:contains the number of arguments
+ *@argv:contains the arguments as strings
+ *Return:Returns 1 on error, 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+int num1, num2;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+printf("%d\n", num1 *num2);
+
+return (0);
+}
