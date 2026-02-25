e4c948a9c9bb7c442ea7a98a7c0ff0f4abf783f8
bfrasholb <13065@holbertonstudents.com>
2026-02-25 00:57:43 +0000

    Autoupdate

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main-prints the function name.
+ *@argc:index of arguments
+ *@argv:null variable
+ *Return:returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
