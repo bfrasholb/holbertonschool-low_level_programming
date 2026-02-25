89b7e19a55a4f06515e8641bf70610af58fba581
bfrasholb <13065@holbertonstudents.com>
2026-02-25 01:48:38 +0000

    Autoupdate

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,30 +2,35 @@
 #include <stdlib.h>
 
 /**
- *main-prints the product of any 2 arguments, throws error if argc != 3
+ *is_number-checks if a string is comprised solely of digits
+ *@s:a string passed from main
+ *Return:Returns 1 for numbers, 0 for anything else
+ */
+
+int is_number(char *s)
+{
+while (*s)
+{
+if (*s < '0' || *s > '9' || *s == '\0')
+{
+return (0);
+}
+s++;
+}
+return (1);
+}
+
+/**
+ *main-prints the sum of positive integer arguments
  *@argc:contains the number of arguments
  *@argv:contains the arguments as strings
  *Return:Returns 1 on error, 0 on success
  */
 
-int is_number(char *s)
-{
-  int j = 1;
-  
-  while (s[j] == '\0')
-    {
-      if (s[j] < '0' || s[j] > '9' || s[j] == '\0')
-	{
-	  return (0);
-	}
-      j++;
-    }
-  return (1);
-}
 
 int main(int argc, char *argv[])
 {
-  int sum = 0, i = 1;
+int sum = 0, i = 1;
 
 if (argc < 2)
 {
@@ -33,15 +38,15 @@ printf("0\n");
 return (0);
 }
 
- while (i < argc)
-   {
-     if (!is_number(argv[i]))
-       {
-	 printf("Error\n");
-	 return(1);
-       }
-     sum += atoi(argv[i++]);
-   }
+while (i < argc)
+{
+if (!is_number(argv[i]))
+{
+printf("Error\n");
+return (1);
+}
+sum += atoi(argv[i++]);
+}
 printf("%d\n", sum);
 return (0);
 }
