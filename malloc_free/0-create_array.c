69872f028cb89de03ee4a56b02a6f7364d52bf47
bfrasholb <13065@holbertonstudents.com>
2026-03-09 06:40:38 +0000

    fixed betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,26 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ *create_array-description
+ *@size:size
+ *@c:character
+ *Return:value dependent on result
+ */
+
 char *create_array(unsigned int size, char c)
 {
-  char *arr;
-  unsigned int i = 0;
+char *arr;
+unsigned int i = 0;
 
-  if (size == 0)
-    {
-      return NULL;
-    }
-
-  arr = (char *)malloc(size * sizeof(char));
-  if (arr == NULL)
-    {
-      return NULL;
-    }
-
-  while (i < size)
-    {
-      arr[i] = c;
-      i++;
-    }
-  return arr;
+if (size == 0)
+{
+return (NULL);
+}
+arr = (char *)malloc(size * sizeof(char));
+if (arr == NULL)
+{
+return (NULL);
+}
+while (i < size)
+{
+arr[i] = c;
+i++;
+}
+return (arr);
 }
