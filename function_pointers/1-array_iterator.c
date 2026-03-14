f8a35559ec4f3647ef7445df57dcd38327bdbf93
bfrasholb <13065@holbertonstudents.com>
2026-03-14 05:54:50 +0000

    Autoupdate

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned long int i = 0;
 
-if ((array != NULL) | (action != NULL))
+if ((array == NULL) | (action == NULL))
 {
 return;
 }
