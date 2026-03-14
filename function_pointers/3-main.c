3faa12896b859fef158fa699b94bd18fa50ddaea
bfrasholb <13065@holbertonstudents.com>
2026-03-14 08:08:20 +0000

    Get Betty'd

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,37 +3,38 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- */
+*main-performs operations on two integers
+*@argc:number of CLI args
+*@argv:array of pointers to arg strings
+*Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
-  int a, b, result;
-  int (*op)(int, int);
-  
-  if (argc != 4)
-	     {
-	       printf("Error\n");
-	       return (98);
-	     }
- 
-  op = get_op_func(argv[2]);
-  if (op == NULL)
-    {
-       printf("Error\n");
-       return (99);
-	     }
-  if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
-    {
-      printf("Error\n");
-       return (100);
-	     }
-  
- a = atoi(argv[1]);
- b = atoi(argv[3]);
- result = op(a, b);
- printf("%d\n",result);
- return (result);
+int a, b, result;
+int (*op)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+
+op = get_op_func(argv[2]);
+if (op == NULL)
+{
+printf("Error\n");
+return (99);
+}
+if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+{
+printf("Error\n");
+return (100);
+}
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+result = op(a, b);
+printf("%d\n", result);
+return (0);
 }
