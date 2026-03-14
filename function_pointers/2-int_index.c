dfb0f427a18a68d8c3454c4b0511407ccfab74bc
bfrasholb <13065@holbertonstudents.com>
2026-03-14 06:08:47 +0000

    Autoupdate

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,35 +2,28 @@
 #include "function_pointers.h"
 
 /**
- *int_index-
- *@array:
- *@size:
- *@cmp:
- */
+*int_index-passes each element of an array to a function
+*@array:an array
+*@size:size of the array
+*@cmp:a function pointer
+*Return:Returns the index of a true value from pointed function, or -1.
+*/
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-  int i = 0;
-  
-  if (size <= 0)
-    {
-      return (-1);
-     }
-  if ((array == NULL) | (cmp == NULL))
-    {
-      return (-1);
-     }
-  
-    while (i < size)
-      {
-	if(cmp(array[i]) != 0)
-	  {
-	  return (i);
-	  }
-	else
-	  {
-	    return (-1);
-	  }
-	i++;
-      }
+int i = 0;
+
+if ((array == NULL) || (cmp == NULL) || (size <= 0))
+{
+return (-1);
+}
+while (i < size)
+{
+if (cmp(array[i]) != 0)
+{
+return (i);
+}
+i++;
+}
+return (-1);
 }
