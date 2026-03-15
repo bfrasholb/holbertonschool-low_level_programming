66730a9160961afc87d84334707f05c7134520f3
bfrasholb <13065@holbertonstudents.com>
2026-03-15 04:30:06 +0000

    Autoupdate

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,28 +1,29 @@
 #include "variadic_functions.h"
 
 /**
- *print_strings-prints strings separated by a separator
- *@separator:a separator string
- *@n:arg count
- */
+*print_strings-prints strings separated by a separator
+*@separator:a separator string
+*@n:arg count
+*/
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-  va_list args;
-  unsigned int i = 0;
-  char *s = va_arg(args, char *);
+va_list args;
+unsigned int i = 0;
+char *s;
 
-  va_start(args, n);
-  while (i < n)
-    {
-      if ((i != 0) && (separator != NULL))
-	printf("%s", separator);
-      if (s != NULL)
-	printf("%s", va_arg(args, char *));
-      else
-	printf("nil");
-      i++;
-    }
-  va_end(args);
-  printf("\n");
+va_start(args, n);
+while (i < n)
+{
+s = va_arg(args, char *);
+if ((i != 0) && (separator != NULL))
+printf("%s", separator);
+if (s != NULL)
+printf("%s", s);
+else
+printf("nil");
+i++;
+}
+va_end(args);
+printf("\n");
 }
