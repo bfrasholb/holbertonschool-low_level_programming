b35699f276b943996aad81f7e5ea073677b31611
bfrasholb <13065@holbertonstudents.com>
2026-03-15 05:46:20 +0000

    Autoupdate

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,4 +7,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
+/**
+ *struct printer-a table linking types of arguments to printer functions
+ *@symbol:symbol for type
+ *@func:printer function respective to type
+ */
+
+typedef struct printer
+{
+  char symbol;
+  void (*func)(va_list);
+} printer_t;
+
 #endif
