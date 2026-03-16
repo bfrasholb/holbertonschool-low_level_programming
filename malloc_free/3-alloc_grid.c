b960bc19ea982b69e2139586867ae24954faae72
bfrasholb <13065@holbertonstudents.com>
2026-03-16 10:21:15 +0000

    Autoupdate

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,40 +1,43 @@
 #include "main.h"
 
 /**
- */
+*alloc_grid-allocates a grid and sets the elements to zero
+*@width:integer width
+*@height:integer height
+*Return:NULL on error, grid on success
+*/
 
 int **alloc_grid(int width, int height)
 {
-  int **grid;
-  int i = 0, j;
+int **grid;
+int i = 0, j;
 
-  if (width <= 0 || height <= 0)
-    return (NULL);
-
-  grid = malloc(sizeof(int *) * height);
-  if (grid == NULL)
-    return (NULL);
-  while (i < height)
-    {
-      grid[i] = malloc(sizeof(int) * width);
-      if (grid[i] == NULL)
-	{
-	  for (j = 0; j < i; j++)
-	    free(grid[j]);
-	  free(grid);
-	  return (NULL);
-	}
-      i++;
-    }
-      i = 0, j = 0;
-  while (i < height)
-    {
-      while (j < width)
-	{
-	  grid[i][j] = 0;
-	  j++;
-	}
-      i++;
-    }
-  return (grid);
+if (width <= 0 || height <= 0)
+return (NULL);
+grid = malloc(sizeof(int *) * height);
+if (grid == NULL)
+return (NULL);
+while (i < height)
+{
+grid[i] = malloc(sizeof(int) * width);
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+free(grid[j]);
+free(grid);
+return (NULL);
+}
+i++;
+}
+i = 0, j = 0;
+while (i < height)
+{
+while (j < width)
+{
+grid[i][j] = 0;
+j++;
+}
+i++;
+}
+return (grid);
 }
