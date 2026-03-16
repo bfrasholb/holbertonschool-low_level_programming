9954b957a76898fae6791f51f76204999c8e8d52
bfrasholb <13065@holbertonstudents.com>
2026-03-16 09:51:55 +0000

    Autoupdate

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,43 +1,41 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
-* str_concat - concatenates two strings
-* @s1: first string
-* @s2: second string
-*
-* Return: pointer to the new concatenated string, or NULL if it fails
+*str_concat-concatenates two strings
+*@s1:a string
+*@s2:a second string :O
+*Return:NULL if failed, the concatenated string on success
 */
+
 char *str_concat(char *s1, char *s2)
 {
-char *newstr;
-int i, j, len1, len2;
+char *result;
+int i = 0, len1 = 0, len2 = 0;
 
 if (s1 == NULL)
 s1 = "";
-
 if (s2 == NULL)
 s2 = "";
-
-len1 = 0;
 while (s1[len1] != '\0')
 len1++;
-
-len2 = 0;
 while (s2[len2] != '\0')
 len2++;
-
-newstr = malloc(sizeof(char) * (len1 + len2 + 1));
-if (newstr == NULL)
+result = (char *)malloc(sizeof(char) * (len1 + len2));
+if (result == NULL)
 return (NULL);
-
-for (i = 0; i < len1; i++)
-newstr[i] = s1[i];
-
-for (j = 0; j < len2; j++)
-newstr[i + j] = s2[j];
-
-newstr[len1 + len2] = '\0';
-
-return (newstr);
+while (i < (len1 + len2))
+{
+if (i < len1)
+{
+result[i] = s1[i];
+i++;
+}
+else
+{
+result [i] = s2[i - len1];
+i++;
+}
+}
+result[i] = '\0';
+return (result);
 }
