e306de2969954b3ce7445e2837115adc57b06332
bfrasholb <13065@holbertonstudents.com>
2026-03-17 01:33:42 +0000

    Autoupdate

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,19 +2,16 @@
 #include "dog.h"
 
 /**
-* free_dog - frees a dog_t structure
-* @d: pointer to the dog to free
+*free_dog-frees a dog_t structure
+*@d:pointer to the dog to free
 */
 void free_dog(dog_t *d)
 {
 if (d == NULL)
 return;
-
 if (d->name != NULL)
 free(d->name);
-
 if (d->owner != NULL)
 free(d->owner);
-
 free(d);
 }
