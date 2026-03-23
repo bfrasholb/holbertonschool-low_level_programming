8787f2e0c53d099c275854a8b24d71f4088c1799
bfrasholb <13065@holbertonstudents.com>
2026-03-23 21:11:17 +1100

    Autoupdate

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * list_len-returns the length of a list
+ * @h:a list
+ * Return:number of elements of the list
+ */
+
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
