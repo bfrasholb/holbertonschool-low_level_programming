2290a57d85479b779c46dc21734300e078851cfc
bfrasholb <13065@holbertonstudents.com>
2026-03-23 20:17:20 +1100

    not bettied

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list- prints a list
+ * @h:head
+ */
+
+size_t print_list(const list_t *h)
+{
+    const list_t *current = h;
+    size_t count = 0;
+
+    while (current != NULL)
+    {
+    if (current->str != NULL)
+    {
+    printf("[%u] %s\n", current->len, current->str);
+    }
+    else
+    {
+        printf("[0] (nil)\n");
+    }
+    current = current->next;
+    count ++;
+    }
+    return (count);
+}
