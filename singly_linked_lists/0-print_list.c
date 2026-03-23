1922dcbaf293deaef8792c8d4879e86496930456
bfrasholb <13065@holbertonstudents.com>
2026-03-23 20:22:23 +1100

    Autoupdate

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,21 +7,21 @@
 
 size_t print_list(const list_t *h)
 {
-    const list_t *current = h;
-    size_t count = 0;
+const list_t *current = h;
+size_t count = 0;
 
-    while (current != NULL)
-    {
-    if (current->str != NULL)
-    {
-    printf("[%u] %s\n", current->len, current->str);
-    }
-    else
-    {
-        printf("[0] (nil)\n");
-    }
-    current = current->next;
-    count ++;
-    }
-    return (count);
+while (current != NULL)
+{
+if (current->str != NULL)
+{
+printf("[%u] %s\n", current->len, current->str);
+}
+else
+{
+printf("[0] (nil)\n");
+}
+current = current->next;
+count ++;
+}
+return (count);
 }
