4513eef3d098f3ceec149a51bd909775e73fe4bc
bfrasholb <13065@holbertonstudents.com>
2026-03-23 21:30:56 +1100

    no strlen?

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,11 +11,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
+	int i = 0;
 
 	if (new == NULL)
 		return (NULL);
+	
+	if (str[i] != '\0')
+	{
+		while (str[i] != '\0')
+			i++;
+	}
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = i;
 	new->next = *head;
 	*head = new;
 	return (new);
