4fbf6ea301155a4cfef2fe4e7a14d3fdaee49991
bfrasholb <13065@holbertonstudents.com>
2026-03-28 12:37:59 +1100

    Betty

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -6,8 +6,8 @@
  * Return: the number of elements in the list
  */
 
- size_t dlistint_len(const dlistint_t *h)
- {
+size_t dlistint_len(const dlistint_t *h)
+{
 	const dlistint_t *current = h;
 	size_t count = 0;
 
@@ -20,4 +20,4 @@
 		current = current->next;
 	}
 	return (count);
- }
+}
