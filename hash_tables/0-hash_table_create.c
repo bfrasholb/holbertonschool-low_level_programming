1324045be200b11312e524a819743295c2c8c8d5
bfrasholb <13065@holbertonstudents.com>
2026-04-01 20:58:59 +1100

    Autoupdate

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -19,10 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(ht->array))
-		{
-			free(ht);
-			return (NULL);
-		}
+	{
+		free(ht);
+		return (NULL);
+	}
 
 	while (i < size)
 	{
